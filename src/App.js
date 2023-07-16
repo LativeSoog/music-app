9f778e8c8e516bf30d85e9261b2cbbf32a896e2b
Vitaliy <konfet88@gmail.com>
2023-07-17 03:39:04 +0600

    Corrected components SectionTrack, added components SearchLine, SearchTrack

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './style/App.css'
-import { AudioPlayer } from './components/AudioPlayer.js'
-import { SectionTrack } from './components/SectionTrack.js'
 import { NavMenu } from './components/NavMenu.js'
+import { AudioPlayer } from './components/AudioPlayer.js'
+import { SectionTrackList } from './components/SectionTrackList.js'
 
 function App() {
   return (
@@ -19,31 +19,7 @@ function App() {
             </div>
             <NavMenu />
           </nav>
-          <div className="main__centerblock centerblock">
-            <div className="centerblock__search search">
-              <svg className="search__svg">
-                <use xlinkHref="img/icon/sprite.svg#icon-search"></use>
-              </svg>
-              <input
-                className="search__text"
-                type="search"
-                placeholder="Поиск"
-                name="search"
-              ></input>
-            </div>
-            <h2 className="centerblock__h2">Треки</h2>
-            <div className="centerblock__filter filter">
-              <div className="filter__title">Искать по:</div>
-              <div className="filter__button button-author _btn-text">
-                исполнителю
-              </div>
-              <div className="filter__button button-year _btn-text">
-                году выпуска
-              </div>
-              <div className="filter__button button-genre _btn-text">жанру</div>
-            </div>
-            <SectionTrack />
-          </div>
+          <SectionTrackList />
           <div className="main__sidebar sidebar">
             <div className="sidebar__personal">
               <p className="sidebar__personal-name">Sergey.Ivanov</p>
---
+++ src/components/SectionTrackList.js
@@ -0,0 +1,26 @@
+import { PlayList } from './Playlist.js'
+import { SearchLine } from './SearchLine.js'
+import { SearchTrack } from './SearchTrack.js'
+
+export function SectionTrackList(props) {
+  return (
+    <div className="main__centerblock centerblock">
+      <SearchLine />
+      <h2 className="centerblock__h2">Треки</h2>
+      <SearchTrack />
+      <div className="centerblock__content">
+        <div className="content__title playlist-title">
+          <div className="playlist-title__col col01">Трек</div>
+          <div className="playlist-title__col col02">ИСПОЛНИТЕЛЬ</div>
+          <div className="playlist-title__col col03">АЛЬБОМ</div>
+          <div className="playlist-title__col col04">
+            <svg className="playlist-title__svg" alt="time">
+              <use xlinkHref="img/icon/sprite.svg#icon-watch"></use>
+            </svg>
+          </div>
+        </div>
+        <PlayList />
+      </div>
+    </div>
+  )
+}
