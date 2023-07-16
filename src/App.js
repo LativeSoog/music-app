3067b5eba5125eb365d6e80c67a6df081f8d77ec
Vitaliy <konfet88@gmail.com>
2023-07-17 01:55:48 +0600

    Added component SectionTrack

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './style/App.css'
 import { AudioPlayer } from './components/AudioPlayer.js'
-import { PlayList } from './components/Playlist.js'
+import { SectionTrack } from './components/SectionTrack.js'
 import { NavMenu } from './components/NavMenu.js'
 
 function App() {
@@ -42,19 +42,7 @@ function App() {
               </div>
               <div className="filter__button button-genre _btn-text">жанру</div>
             </div>
-            <div className="centerblock__content">
-              <div className="content__title playlist-title">
-                <div className="playlist-title__col col01">Трек</div>
-                <div className="playlist-title__col col02">ИСПОЛНИТЕЛЬ</div>
-                <div className="playlist-title__col col03">АЛЬБОМ</div>
-                <div className="playlist-title__col col04">
-                  <svg className="playlist-title__svg" alt="time">
-                    <use xlinkHref="img/icon/sprite.svg#icon-watch"></use>
-                  </svg>
-                </div>
-              </div>
-              <PlayList />
-            </div>
+            <SectionTrack />
           </div>
           <div className="main__sidebar sidebar">
             <div className="sidebar__personal">
