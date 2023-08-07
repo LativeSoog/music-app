33bae3fb614961a3f5c40257365cd171a70621f2
Vitaliy <konfet88@gmail.com>
2023-08-08 00:58:27 +0600

    Added pages 404, Reg, Favorite and Category

---
+++ src/pages/NotFound/NotFoundPage.jsx
@@ -0,0 +1,7 @@
+export const NotFoundPage = () => {
+  return (
+    <div>
+      <h1>Sorry, 404 - Page not found :c</h1>
+    </div>
+  )
+}
--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,21 @@
 import { Route, Routes } from 'react-router-dom'
 import { LoginPage } from './pages/Login/LoginPage.jsx'
 import { MainPage } from './pages/Main/MainPage.jsx'
+import { NotFoundPage } from './pages/NotFound/NotFoundPage.jsx'
+import { RegistrationPage } from './pages/Registration/RegistrationPage.jsx'
+import { FavoritesPage } from './pages/Favorites/FavoritesPage.jsx'
+import { CategoryPage } from './pages/Category/CategoryPage.jsx'
 
 export const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<MainPage />} />
+      <Route path="*" element={<NotFoundPage />} />
 
       <Route path="/login" element={<LoginPage />} />
+      <Route path="/registration" element={<RegistrationPage />} />
+      <Route path="/favorites" element={<FavoritesPage />} />
+      <Route path="/category" element={<CategoryPage />} />
     </Routes>
   )
 }
