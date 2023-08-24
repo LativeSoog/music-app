a9ba89be9165e8761245085b237bcd8d508c77c2
Vitaliy <konfet88@gmail.com>
2023-08-25 02:22:05 +0600

    Edited login page, create registration page

--- src/pages/Login/LoginPage.jsx
+++ src/pages/Login/LoginPage.jsx
@@ -2,27 +2,17 @@ import { Link } from 'react-router-dom'
 import * as S from './LoginPageStyles.js'
 import { useEffect, useState } from 'react'
 
-export function LoginPage({ isLoginMode = false }) {
+export function LoginPage() {
   const [error, setError] = useState(null)
 
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
-  const [repeatPassword, setRepeatPassword] = useState('')
 
   const handleLogin = async ({ email, password }) => {
     alert(`Выполняется вход: ${email} ${password}`)
     setError('Неизвестная ошибка входа')
   }
 
-  const handleRegister = async () => {
-    alert(`Выполняется регистрация: ${email} ${password}`)
-    setError('Неизвестная ошибка регистрации')
-  }
-
-  // Сбрасываем ошибку если пользователь меняет данные на форме или меняется режим формы
-  useEffect(() => {
-    setError(null)
-  }, [isLoginMode, email, password, repeatPassword])
 
   return (
     <S.PageContainer>
@@ -32,77 +22,35 @@ export function LoginPage({ isLoginMode = false }) {
             <S.ModalLogoImage src="/img/logo_modal.png" alt="logo" />
           </S.ModalLogo>
         </Link>
-        {isLoginMode ? (
-          <>
-            <S.Inputs>
-              <S.ModalInput
-                type="text"
-                name="login"
-                placeholder="Почта"
-                value={email}
-                onChange={(event) => {
-                  setEmail(event.target.value)
-                }}
-              />
-              <S.ModalInput
-                type="password"
-                name="password"
-                placeholder="Пароль"
-                value={password}
-                onChange={(event) => {
-                  setPassword(event.target.value)
-                }}
-              />
-            </S.Inputs>
-            {error && <S.Error>{error}</S.Error>}
-            <S.Buttons>
-              <S.PrimaryButton onClick={() => handleLogin({ email, password })}>
-                Войти
-              </S.PrimaryButton>
-              <Link to="/register">
-                <S.SecondaryButton>Зарегистрироваться</S.SecondaryButton>
-              </Link>
-            </S.Buttons>
-          </>
-        ) : (
-          <>
-            <S.Inputs>
-              <S.ModalInput
-                type="text"
-                name="login"
-                placeholder="Почта"
-                value={email}
-                onChange={(event) => {
-                  setEmail(event.target.value)
-                }}
-              />
-              <S.ModalInput
-                type="password"
-                name="password"
-                placeholder="Пароль"
-                value={password}
-                onChange={(event) => {
-                  setPassword(event.target.value)
-                }}
-              />
-              <S.ModalInput
-                type="password"
-                name="repeat-password"
-                placeholder="Повторите пароль"
-                value={repeatPassword}
-                onChange={(event) => {
-                  setRepeatPassword(event.target.value)
-                }}
-              />
-            </S.Inputs>
-            {error && <S.Error>{error}</S.Error>}
-            <S.Buttons>
-              <S.PrimaryButton onClick={handleRegister}>
-                Зарегистрироваться
-              </S.PrimaryButton>
-            </S.Buttons>
-          </>
-        )}
+        <S.Inputs>
+          <S.ModalInput
+            type="text"
+            name="login"
+            placeholder="Почта"
+            value={email}
+            onChange={(event) => {
+              setEmail(event.target.value)
+            }}
+          />
+          <S.ModalInput
+            type="password"
+            name="password"
+            placeholder="Пароль"
+            value={password}
+            onChange={(event) => {
+              setPassword(event.target.value)
+            }}
+          />
+        </S.Inputs>
+        {error && <S.Error>{error}</S.Error>}
+        <S.Buttons>
+          <S.PrimaryButton onClick={() => handleLogin({ email, password })}>
+            Войти
+          </S.PrimaryButton>
+          <Link to="/registration">
+            <S.SecondaryButton>Зарегистрироваться</S.SecondaryButton>
+          </Link>
+        </S.Buttons>
       </S.ModalForm>
     </S.PageContainer>
   )
--- src/pages/Registration/RegistrationPage.jsx
+++ src/pages/Registration/RegistrationPage.jsx
@@ -1,7 +1,64 @@
+import { Link } from 'react-router-dom'
+import * as S from './RegistrationPageStyles.js'
+import { useState } from 'react'
+
 export const RegistrationPage = () => {
+  const [error, setError] = useState(null)
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [repeatPassword, setRepeatPassword] = useState('')
+
+  const handleRegister = async () => {
+    alert(`Выполняется регистрация: ${email} ${password}`)
+    setError('Неизвестная ошибка регистрации')
+  }
+
   return (
-    <div>
-      <h1>Registration Page</h1>
-    </div>
+    <S.PageContainer>
+      <S.ModalForm>
+        <Link to="/registration">
+          <S.ModalLogo>
+            <S.ModalLogoImage src="/img/logo_modal.png" alt="logo" />
+          </S.ModalLogo>
+        </Link>
+
+        <S.Inputs>
+          <S.ModalInput
+            type="text"
+            name="login"
+            placeholder="Почта"
+            value={email}
+            onChange={(event) => {
+              setEmail(event.target.value)
+            }}
+          />
+          <S.ModalInput
+            type="password"
+            name="password"
+            placeholder="Пароль"
+            value={password}
+            onChange={(event) => {
+              setPassword(event.target.value)
+            }}
+          />
+          <S.ModalInput
+            type="password"
+            name="repeat-password"
+            placeholder="Повторите пароль"
+            value={repeatPassword}
+            onChange={(event) => {
+              setRepeatPassword(event.target.value)
+            }}
+          />
+        </S.Inputs>
+        {error && <S.Error>{error}</S.Error>}
+        <S.Buttons>
+          <S.PrimaryButton onClick={handleRegister}>
+            Зарегистрироваться
+          </S.PrimaryButton>
+        </S.Buttons>
+      </S.ModalForm>
+    </S.PageContainer>
   )
 }
